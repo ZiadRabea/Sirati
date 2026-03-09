aee6ef236e04cb35d86f4edd6d3cf8f33a4c1115
Ziad Rabea <138338114+ZiadRabea@users.noreply.github.com>
2026-03-09 23:16:34 +0200

    update

--- portfolio/helpers.py
+++ portfolio/helpers.py
@@ -7,9 +7,17 @@ import os
 def is_valid_signature(raw_body, received_sig):
     print(f"Received Signature : {received_sig}")
     print(f"raw body: {raw_body}")
-
+    secret_key = os.environ.get("MID")
     data = raw_body.get("signatureKeys")
     
     concat_values = "&".join(f"{key}={raw_body.get(key,'')}" for key in data)
     
     print(f"Concat Values : {concat_values}" if concat_values else "NO Concat Values")
+
+    computed_sig = hmac.new(
+        secret_key.encode("utf-8"),
+        concat_values.encode("utf-8"),
+        hashlib.sha256
+    ).hexdigest()
+
+    print(computed_sig)
