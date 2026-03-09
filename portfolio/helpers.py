7fe5add44debb71f8e0f1b5372fd46fecf3e343d
Ziad Rabea <138338114+ZiadRabea@users.noreply.github.com>
2026-03-09 22:09:43 +0200

    update

--- portfolio/helpers.py
+++ portfolio/helpers.py
@@ -5,26 +5,7 @@ import os
 
 
 def is_valid_signature(raw_body, received_sig):
-    secret = os.environ.get("KASHIER_SECRET")
-    if not secret or not received_sig:
-        return False
-
-    body = json.loads(raw_body)
-    data = body.get("data", {})
-
-    signature_keys = data.get("signatureKeys", [])
-
-    parts = []
-    for key in signature_keys:
-        value = data.get(key, "")
-        parts.append(f"{key}={value}")
-
-    message = "&".join(parts)
-
-    computed_sig = hmac.new(
-        secret.encode("ascii"),
-        message.encode("ascii"),
-        hashlib.sha256
-    ).hexdigest().lower()
+    print("Received Signature : " received_sig)
+    print("raw body": raw_body)
 
     return hmac.compare_digest(computed_sig, received_sig.lower())
