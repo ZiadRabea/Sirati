57558c3313b19ef33f8661125a270700683fd053
Ziad Rabea <138338114+ZiadRabea@users.noreply.github.com>
2026-03-09 22:18:19 +0200

    update

--- portfolio/helpers.py
+++ portfolio/helpers.py
@@ -6,6 +6,6 @@ import os
 
 def is_valid_signature(raw_body, received_sig):
     print("Received Signature : " received_sig)
-    print("raw body": raw_body)
+    print("raw body:" raw_body)
 
     return hmac.compare_digest(computed_sig, received_sig.lower())
