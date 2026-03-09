93cff5cd3e64e1097a66391124af3b035c2a0233
Ziad Rabea <138338114+ZiadRabea@users.noreply.github.com>
2026-03-09 22:31:01 +0200

    update

--- portfolio/helpers.py
+++ portfolio/helpers.py
@@ -7,4 +7,7 @@ import os
 def is_valid_signature(raw_body, received_sig):
     print(f"Received Signature : {received_sig}")
     print(f"raw body: {raw_body}")
+
+    data = raw_body.get("signatureKeys")
+    print(data)
     return None
